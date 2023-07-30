3de83de050ed12c329c660c8da6ec2ea6aa7f70c
aley amr <aleyamr2004@gmail.com>
2023-07-30 03:48:29 +0300

    first commit

--- app.js
+++ app.js
@@ -152,7 +152,6 @@ app.get("/work", function(req,res){
 app.get("/about", function(req, res){
   res.render("about");
 });
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT||3000, function() {
+  console.log("Server is running on port 3000");
 });
